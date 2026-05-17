705e43401d289cf5db2f2f9f45e2041b8ac82d00
Dipokdas347250 <dipakdas24680@gmail.com>
2026-05-18 02:09:50 +0600

    navber fixed

--- src/components/common/Navber.jsx
+++ src/components/common/Navber.jsx
@@ -8,6 +8,8 @@ import Navberdata from '@/data/navber';
 import Link from 'next/link';
 import { FaBarsStaggered } from "react-icons/fa6";
 import { GiTireIronCross } from "react-icons/gi";
+import { GoSearch } from "react-icons/go";
+
 
 
 
@@ -24,7 +26,7 @@ const Navber = () => {
                                 <Image src={logo} alt="logo" />
                             </Link>
                         </div>
-                        <div className=" flex items-center gap-8">
+                        <div className=" flex items-center gap-20">
                             <div className="">
                                 <ul className='flex items-center gap-10'>
                                     {Navberdata?.navlist?.map((item, index) => (
@@ -47,18 +49,25 @@ const Navber = () => {
                                 </ul>
                             </div>
                             <div className="flex items-center gap-5">
-                                <div className="p-2.5 bg-secondary text-white border border-secondary rounded-full cursor-pointer duration-300 ease-in-out hover:bg-white hover:text-secondary">
-                                    <FaRegHeart />
-                                </div>
+                                
                                 <div className="p-2.5 bg-secondary text-white border border-secondary rounded-full cursor-pointer duration-300 ease-in-out hover:bg-white hover:text-secondary">
                                     <FaRegUser />
                                 </div>
                                 <div className="p-2.5 bg-secondary text-white border border-secondary rounded-full cursor-pointer duration-300 ease-in-out hover:bg-white hover:text-secondary">
                                     <PiShoppingCart />
                                 </div>
+                                <div className="p-2.5 bg-secondary text-white border border-secondary rounded-full cursor-pointer duration-300 ease-in-out hover:bg-white hover:text-secondary">
+                                    <GoSearch />
+                                </div>
+                            {/* <label className=' pl-3 py-1 pr-1 bg-[#F4F4F4] flex justify-between items-center rounded-[10px]' >
+                                    <input className=' outline-none w-[50%]' type="email" placeholder=' Enter email address' />
+                                    <button className='bg-secondary border border-secondary text-white font-nunito font-bold text-[14px] px-5.5 py-2.5 uppercase cursor-pointer duration-300 ease-in-out hover:bg-white hover:text-secondary rounded-[10px]'><GoSearch /></button>
+
+                                </label> */}
                             </div>
                             <div className="">
-                                <button className='bg-secondary border border-secondary text-white font-nunito font-bold text-[14px] px-7.5 py-4 uppercase cursor-pointer duration-300 ease-in-out hover:bg-white hover:text-secondary'>Get A Quote</button>
+                                <FaBarsStaggered/>
+                                <GiTireIronCross/>
                             </div>
                         </div>
                     </div>
