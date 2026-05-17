85557ec0721f3a4eb1dd1b8b6870f190e0ec633e
Dipokdas347250 <dipakdas24680@gmail.com>
2026-05-18 03:37:43 +0600

    VISIT OUR SHOP responsive done

--- src/components/home/Visit_our_Shop.jsx
+++ src/components/home/Visit_our_Shop.jsx
@@ -8,14 +8,14 @@ import Link from 'next/link';
 const Visit_our_Shop = () => {
     return (
         <>
-            <section className=' py-17.5 bg-[#525355] relative'>
-                <Image className=' absolute top-47.5 left-0 ' src={needleft} alt="needleft"  /> 
+            <section className=' lg:py-17.5 py-10 px-2 bg-[#525355] relative'>
+                <Image className=' absolute lg:top-47.5 left-0 lg:w-[8%] w-[10%] ' src={needleft} alt="needleft"  /> 
                 <Container>
-                    <div className=" flex justify-between items-center">
-                        <div className=" w-1/2 ">
-                            <div className="py-30">
+                    <div className=" lg:flex justify-between items-center">
+                        <div className="lg:w-1/2 w-full">
+                            <div className="lg:py-30 py-10">
                                 <h4 className='text-[18px] text-secondary font-bold font-nunito'>VISIT OUR SHOP</h4>
-                                <h2 className='text-[50px] text-white font-bold font-inter leading-[120%]'>Need Any Organic Fresh Food ?</h2>
+                                <h2 className='lg:text-[50px] text-[32px] text-white font-bold font-inter leading-[120%]'>Need Any Organic Fresh Food ?</h2>
                                 <p className='text-[18px] text-white font-normal font-nunito w-[90%] mt-5'>Cur tantas regiones barbarorum obiit, tot maria transmist summo bono fruitur id est voluptate barbarorum   </p>
 
                                 <div className="flex gap-5 items-center mt-7.5">
@@ -28,7 +28,7 @@ const Visit_our_Shop = () => {
 
                             </div>
                         </div>
-                        <div className="w-1/3">
+                        <div className="lg:w-1/3 w-full">
                             <Image className=' w-full' src={need} alt="need" />
 
                         </div>
